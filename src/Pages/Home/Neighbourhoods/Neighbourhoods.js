89ab7e1c28689007284f52ed857dd3c8ceea59f9
tanjid111 <tanjidhossain@gmail.com>
2022-04-16 17:57:24 -0700

    comp updated

---
+++ src/Pages/Home/Neighbourhoods/Neighbourhoods.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Neighbourhood = () => {
+    return (
+        <div>
+            <h2>This is list of Neighbourhoods</h2>
+        </div>
+    );
+};
+
+export default Neighbourhood; <h2>This is Neighbourhood</h2>
